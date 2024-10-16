863945442a58b3507c6970815f78a657c539b46b
JoeyScottSchronce <joeyscottschronce@gmail.com>
2024-10-16 15:57:45 -0400

    update config files

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: "/Skills-Network-Environment_vite_react_lab",
   plugins: [react()],
 })
